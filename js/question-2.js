23cd18589dc1329640e672f35a44b01f5cab6874
KThomiss <79108090+KThomiss@users.noreply.github.com>
2021-08-27 19:13:24 +0200

    Second day trying to figure it out
    Mosts of the tasks are done, still need to figure out how to display loading animation while waiting for the API call to go through.

--- js/question-2.js
+++ js/question-2.js
@@ -8,35 +8,41 @@ const proxy = corsUrl + apiUrl;
     "x-rapidapi-key": "20322fc9f1d848dcb8c05015fd877883",
 }};*/
 
-const resultsContainer = document.querySelector(".result");
+const nameContainer = document.querySelector(".name");
+const ratingContainer = document.querySelector(".rating");
+const tagsContainer = document.querySelector(".tags");
 
 const errorContainer = document.querySelector(".error");
 
 async function getGames() {
     try {
         const response = await fetch(proxy);
-        const results = await response.json();
+        const apiArray = await response.json();
 
-        console.log(results);
+        console.log(apiArray);
 
-        //const games = results.name;
+        const facts = apiArray.results;
 
-        //resultsContainer.innerHTML = "";
+        //nameContainer.innerHTML = "";
 
-        for (let i = 0; i < results.length; i++) {
-            console.log(results[i].name);
+        for (let i = 0; i < facts.length; i++) {
+            console.log(facts[i].name);
+            console.log(facts[i].rating);
+            console.log(facts.length);
 
-            resultsContainer.innerHTML += `<div class="result">${results[i].name}</div>`;
+            if (i === 8) {
+                break;
+            }
+
+            nameContainer.innerHTML += `<div class="name">${facts[i].name}</div>`;
+            ratingContainer.innerHTML += `<div class="rating">${facts[i].rating}</div>`;
+            tagsContainer.innerHTML += `<div class="tags">${facts.length}</div>`;
         }
-        //console.log(results[i].name);
-
     } catch (error) {
         console.log("an error occurred");
         errorContainer.innerHTML = apiError("An error occurred when calling the API");
+    } finally {
+        console.log("Everything is OK");
     }
 }
 getGames();
-
-// Working on getting the array to be displayed on the page
-// What is the property of the array?
-// Component script for default function to handle array list?
